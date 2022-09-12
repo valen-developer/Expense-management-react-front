3c6ee1441c8a15ed2017372b98ed576d66e214d3
Valentín Valverde Romero <valenreta.developer@gmail.com>
2022-09-12 20:29:53 +0200

    feat($config): cleaning src workspace

---
+++ src/presentation/App.tsx
@@ -0,0 +1,11 @@
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <h1>Gestión de gastos</h1>
+    </div>
+  );
+}
+
+export default App;
